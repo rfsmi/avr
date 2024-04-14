6bf65a86616e8a385732ade800f749a97a8bc3bb
Ryan Smith <ryan.smith220@gmail.com>
2024-04-14 02:36:02 +0000

    Blinking light

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 avr-objcopy -O ihex target/avr-attiny85/debug/avr.elf target/avr-attiny85/debug/avr.hex
+
+avrdude -p t85 -P usb -u -c usbasp -U flash:w:avr.elf:a
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
 #![no_std]
 #![no_main]
-
 #![feature(abi_avr_interrupt)]
 
 use avr_device::interrupt;
 use avr_device::interrupt::{free, Mutex};
-use panic_halt as _;
 use core::cell::RefCell;
 
-type InterruptFlag = Mutex<RefCell<bool>>;
-static TIMER_INTERRUPT: InterruptFlag = Mutex::new(RefCell::new(false));
+use panic_halt as _;
+
+static SHOW_LED: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
 
 #[avr_device::entry]
 fn main() -> ! {
     let dp = avr_device::attiny85::Peripherals::take().unwrap();
     dp.PORTB.ddrb.modify(|_, w| w.pb3().set_bit());
-    dp.PORTB.portb.modify(|_, w| w.pb3().bit(true));
 
-    // Enable global interrupts. Last thig you do.
+    // Configure timer/counter 0 to count up and fire the TIMER0_COMPA at a
+    // regular interval to act as a clock for our timers The compare interrupt
+    // is set to fire roughly every 1ms: 1 / (1Mhz / 8) * 125 = 1ms
+    dp.TC0.tccr0a.write(|w| w.wgm0().ctc());
+    dp.TC0.tccr0b.write(|w| w.cs0().prescale_1024());
+    dp.TC0.ocr0a.write(|w| w.bits(124));
+    dp.TC0.timsk.write(|w| w.ocie0a().bit(true));
+
+    // Lastly enable global interrupts
     unsafe { avr_device::interrupt::enable() };
-    
+
     loop {
-        // dp.PORTB.pinb.modify(|_, w| w.pb3().bit(true));
+        free(|cs| {
+            let led_on = *SHOW_LED.borrow(cs).borrow();
+            dp.PORTB.portb.modify(|_, w| w.pb3().bit(led_on));
+        });
+        avr_device::asm::sleep();
     }
 }
 
 #[interrupt(attiny85)]
 fn TIMER0_COMPA() {
     free(|cs| {
-        TIMER_INTERRUPT.borrow(cs).replace(true);
+        // Just toggle the led
+        SHOW_LED.borrow(cs).replace_with(|&mut old| !old);
     })
 }
