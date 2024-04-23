74aee5f4f9815b45b0f10cbc05ec5972bcff8831
Ryan Smith <ryan.smith220@gmail.com>
2024-04-23 04:56:29 +0000

    hello world!

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,20 @@
 
 use avr_device::interrupt;
 use avr_device::interrupt::{free, CriticalSection, Mutex};
-use core::arch::asm;
-use core::borrow::{Borrow, BorrowMut};
-use core::cell::{Cell, RefCell, UnsafeCell};
-use core::iter::*;
+use core::cell::Cell;
 use core::mem::MaybeUninit;
-use core::ops::DerefMut;
 use embedded_hal::delay::DelayNs;
-use itertools::*;
 
 use attiny_hal as hal;
-use hal::delay::Delay;
 
 use panic_halt as _;
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum UsiState {
+    Available,
+    NextByte(u8),
+    Waiting,
+}
 
 static USI_STATE: Mutex<Cell<UsiState>> = Mutex::new(Cell::new(UsiState::Available));
 
@@ -29,137 +30,45 @@ fn peripherals<'cs>(cs: CriticalSection<'cs>) -> &'cs avr_device::attiny85::Peri
     unsafe { (*PERIPHERALS.borrow(cs).as_ptr()).assume_init_ref() }
 }
 
-#[avr_device::entry]
-fn main() -> ! {
-    // Initialize the static PERIPHERALS
-    free(|cs| {
-        let peripherals = avr_device::attiny85::Peripherals::take().unwrap();
-        unsafe { (*PERIPHERALS.borrow(cs).as_ptr()).write(peripherals) }
-    });
+#[inline(always)]
+fn send_byte(byte: u8) {
+    loop {
+        if free(|cs| {
+            let dp = peripherals(cs);
+            let output = USI_STATE.borrow(cs);
+            if output.get() != UsiState::Available {
+                return false;
+            }
+            let reversed = byte.reverse_bits();
+            let b1 = reversed >> 1;
+            let b2 = reversed << 6 | 0b00111111;
+            output.set(UsiState::NextByte(b2));
 
-    // Configure timer/counter 0 to count up and fire the TIMER0_COMPA at a
-    // regular interval to act as a clock for our timers The compare interrupt
-    // is set to fire roughly every 1ms: 1 / (1Mhz / 8) * 125 = 1ms
+            // Reset clock
+            dp.TC0.tccr0a.write(|w| w.wgm0().ctc());
+            dp.TC0.tccr0b.write(|w| w.cs0().direct());
+            dp.TC0.gtccr.write(|w| w.psr0().set_bit());
+            dp.TC0.tcnt0.write(|w| w.bits(0));
+            dp.TC0.ocr0a.write(|w| w.bits(104));
 
-    free(|cs| {
-        let dp = peripherals(cs);
-
-        dp.PORTB.ddrb.modify(|_, w| {
-            w.pb0().set_bit();
-            w.pb1().set_bit();
-            w.pb2().set_bit();
-            w.pb3().set_bit();
-            w.pb4().set_bit();
-            w.pb5().set_bit()
-        });
-        dp.PORTB.portb.modify(|_, w| {
-            w.pb0().clear_bit();
-            w.pb1().set_bit();
-            w.pb2().clear_bit();
-            w.pb3().clear_bit();
-            w.pb4().clear_bit();
-            w.pb5().clear_bit()
-        });
-    });
-
-    // Flip pin every cycle
-    // dp.TC1.tccr1.write(|w| w.ctc1().set_bit().cs1().direct());
-    // dp.TC1.ocr1c.write(|w| w.bits(0)); // Every cycle
-    // dp.TC1.gtccr.write(|w| w.com1b().match_toggle());
-
-    // Lastly enable global interrupts
-    unsafe { avr_device::interrupt::enable() };
-    // DEBUG.write(b"hello world!");
-
-    // Send byte via USI
-    free(|cs| {
-        let dp = peripherals(cs);
-
-        // Put timer into sync mode
-        dp.TC0.gtccr.write(|w| {
-            w.tsm().set_bit();
-            w.psr0().set_bit()
-        });
-        // ocr0a = clock / (baud * prescale)
-        // So we end up with 104.17 with 1Mhz clock (no prescale) and 9600 baud
-        dp.TC0.tccr0a.write(|w| w.wgm0().ctc());
-        // dp.TC0.tccr0b.write(|w| w.cs0().direct());
-        // dp.TC0.ocr0a.write(|w| w.bits(104));
-        dp.TC0.tccr0b.write(|w| w.cs0().prescale_8());
-        dp.TC0.ocr0a.write(|w| w.bits(125));
-        // dp.TC0.timsk.write(|w| w.ocie0a().set_bit());
-
-        // Disable sync mode
-        dp.TC0.gtccr.write(|w| {
-            w.tsm().clear_bit();
-            w.psr0().clear_bit()
-        });
-
-        // Configure USI
-        dp.USI.usicr.write(|w| {
-            w.usiwm().three_wire();
-            w.usioie().set_bit();
-            w.usics().tc0()
-        });
-
-        if let UsiState::Transmitting {
-            next_bits: (send_bits, send_bits_length),
-            buffer,
-            data,
-        } = next_transmit_state((0, 0), b"hello world!")
-        {
-            USI_STATE.borrow(cs).set(next_transmit_state(buffer, data));
-            dp.USI.usidr.write(|w| w.bits(send_bits));
+            // Write output
+            dp.PORTB.ddrb.modify(|_, w| w.pb1().set_bit());
+            dp.USI.usidr.write(|w| w.bits(b1));
             dp.USI.usisr.write(|w| {
                 w.usioif().set_bit();
-                w.usicnt().bits(16 - send_bits_length as u8)
+                w.usicnt().bits(9)
             });
+
+            // Configure USI
+            dp.USI.usicr.write(|w| {
+                w.usiwm().three_wire();
+                w.usioie().set_bit();
+                w.usics().tc0()
+            });
+            true
+        }) {
+            break;
         }
-    });
-
-    // for byte in b"hello world!" {
-    // }
-
-    loop {
-        // free(|cs| {
-        //     if let Some(signal) = DEBUG.get_signal(cs) {
-        //         dp.PORTB.portb.write(|w| w.pb3().bit(signal));
-        //     }
-        // });
-    }
-}
-
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum UsiState<'data> {
-    Available,
-    Transmitting {
-        next_bits: (u8, usize),
-        buffer: (u32, usize),
-        data: &'data [u8],
-    },
-}
-
-#[inline(never)]
-fn next_transmit_state((mut buffer, mut buffer_size): (u32, usize), mut data: &[u8]) -> UsiState {
-    if buffer_size < 8 && !data.is_empty() {
-        // Push the start bit
-        buffer_size += 1;
-        // Push the data byte
-        buffer |= (data[0] as u32) << buffer_size;
-        buffer_size += 8;
-        data = &data[1..];
-        // Push the stop bit
-        buffer |= 1 << buffer_size;
-        buffer_size += 1;
-    }
-    let next_bits = (buffer as u8).reverse_bits();
-    let next_bits_length = buffer_size.min(8);
-    buffer_size -= next_bits_length;
-    buffer >>= next_bits_length;
-    UsiState::Transmitting {
-        next_bits: (next_bits, next_bits_length),
-        buffer: (buffer, buffer_size),
-        data,
     }
 }
 
@@ -169,38 +78,66 @@ fn USI_OVF() {
         let dp = peripherals(cs);
         let output = USI_STATE.borrow(cs);
         match output.get() {
-            UsiState::Available => {}
-            UsiState::Transmitting {
-                buffer: (_, 0),
-                data: &[],
-                ..
-            } => {
-                dp.PORTB.portb.write(|w| w.pb1().set_bit());
-                dp.USI.usicr.write(|w| {
-                    w.usiwm().disabled();
-                    w.usioie().clear_bit();
-                    w.usics().no_clock()
-                });
-                dp.USI.usisr.write(|w| w.usioif().set_bit());
-                output.set(UsiState::Available)
-            }
-            UsiState::Transmitting {
-                next_bits: (send_bits, send_bits_length),
-                buffer,
-                data,
-            } => {
-                dp.USI.usidr.write(|w| w.bits(send_bits));
+            UsiState::NextByte(byte) => {
+                dp.USI.usidr.write(|w| w.bits(byte));
                 dp.USI.usisr.write(|w| {
                     w.usioif().set_bit();
-                    w.usicnt().bits(16 - send_bits_length as u8)
+                    w.usicnt().bits(13)
                 });
-                output.set(next_transmit_state(buffer, data))
+                output.set(UsiState::Waiting)
             }
+            UsiState::Waiting => {
+                dp.USI.usicr.reset();
+                dp.PORTB.ddrb.modify(|_, w| w.pb1().set_bit());
+                dp.PORTB.portb.modify(|_, w| w.pb1().set_bit());
+                dp.USI.usisr.modify(|_, w| w.usioif().set_bit());
+                output.set(UsiState::Available)
+            }
+            UsiState::Available => {}
         }
     });
 }
 
-// #[interrupt(attiny85)]
-// fn TIMER0_COMPA() {
-//     DEBUG.tick();
-// }
+#[avr_device::entry]
+fn main() -> ! {
+    // Initialize the peripherals global
+    free(|cs| {
+        let peripherals = avr_device::attiny85::Peripherals::take().unwrap();
+        unsafe { (*PERIPHERALS.borrow(cs).as_ptr()).write(peripherals) }
+    });
+
+    unsafe { avr_device::interrupt::enable() };
+
+    // Initialize all pins to output
+    free(|cs| {
+        let dp = peripherals(cs);
+
+        dp.PORTB.ddrb.write(|w| {
+            w.pb0().set_bit();
+            w.pb1().set_bit();
+            w.pb2().set_bit();
+            w.pb3().set_bit();
+            w.pb4().set_bit();
+            w.pb5().set_bit()
+        });
+        dp.PORTB.portb.write(|w| {
+            w.pb0().clear_bit();
+            w.pb1().set_bit();
+            w.pb2().clear_bit();
+            w.pb3().clear_bit();
+            w.pb4().clear_bit();
+            w.pb5().clear_bit()
+        });
+    });
+
+    // Wait a bit
+    let mut delay = hal::delay::Delay::<hal::clock::MHz1>::new();
+    delay.delay_ms(500);
+
+    // Send a message
+    for &byte in b"hello world!" {
+        send_byte(byte);
+    }
+
+    loop {}
+}
